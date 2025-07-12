141c6d186e676a3bdd5d7384be2e61977f8b0987
cgarling <chris.t.garling@gmail.com>
2025-07-12 11:02:32 -0400

    Start implementing ATLAS9 BCs

---
+++ test/YBC/atlas9_tests.jl
@@ -0,0 +1,11 @@
+using Test: @test
+using BolometricCorrections
+
+grid = ATLAS9YBCGrid("acs_wfc")
+for mh in range(extrema(BolometricCorrections.YBC.ATLAS9.gridinfo.MH)...; step=0.1)
+    for Av in range(extrema(BolometricCorrections.YBC.ATLAS9.gridinfo.Av)...; step=0.1)
+        table = grid(mh, Av)
+        @test table isa ATLAS9YBCTable
+        @test table(4250, 1.0) isa AbstractVector # technically SVector but ...
+    end
+end
--- test/YBC/run_ybc_tests.jl
+++ test/YBC/run_ybc_tests.jl
@@ -1,3 +1,4 @@
 @testset verbose=true "YBC Submodule" begin
     @safetestset "PHOENIX" include("phoenix_tests.jl")
+    @safetestset "ATLAS9" include("atlas9_tests.jl")
 end
