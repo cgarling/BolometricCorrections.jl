9e42991459dea081d4e5d2cb3a0cda8503b90587
cgarling <chris.t.garling@gmail.com>
2025-07-24 18:53:37 -0400

    Test output types when interpolating
    Important these are uniform for `isochrone` methods in StellarTracks.jl

--- test/YBC/koester_tests.jl
+++ test/YBC/koester_tests.jl
@@ -1,5 +1,6 @@
 using Test: @test
 using BolometricCorrections.YBC.KoesterWD: KoesterWDYBCGrid, KoesterWDYBCTable, gridinfo, chemistry
+using StaticArrays: SVector
 
 @test ismissing(chemistry(KoesterWDYBCGrid))
 @test ismissing(chemistry(KoesterWDYBCTable))
@@ -8,5 +9,5 @@ grid = KoesterWDYBCGrid("acs_wfc")
 for Av in range(extrema(gridinfo.Av)...; step=0.1)
     table = grid(Av)
     @test table isa KoesterWDYBCTable
-    @test table(10_250.0, 7.33) isa AbstractVector # technically SVector but ...
+    @test table(10_250.0, 7.33) isa SVector{12, BolometricCorrections.YBC.dtype}
 end
