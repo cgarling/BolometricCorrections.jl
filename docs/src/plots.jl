226b05382a9047685b3e8040935cf792ccf8625b
cgarling <chris.t.garling@gmail.com>
2025-07-24 15:52:19 -0400

    Experiment with YBC metallicity extrapolation
    Flat metallicity extrapolation seems fine -- add as keyword argument to `YBCGrid`, add struct field, and then use when interpolating `YBCTable`s.

--- docs/src/plots.jl
+++ docs/src/plots.jl
@@ -61,3 +61,26 @@ function plot_bc_table(table::AbstractBCTable, filtername::AbstractString)
     logg = range(extrema(table).logg...; length=1000)
     plot_bc_table(table::AbstractBCTable, filtername::AbstractString, Teff, logg)
 end
+
+function plot_bc_table_diff(table1::AbstractBCTable, table2::AbstractBCTable, fnames::NTuple{2, String}, Teff::AbstractVector, logg::AbstractVector)
+    # We'll plot Teff decreasing from left to right
+    if !issorted(Teff; rev=true)
+        Teff = sort(Teff; rev=true)
+    end
+    logTeff = log10.(Teff)
+    # Transposing logg will evaluate BC for every combination of Teff and logg
+    data1 = table1.(Teff, logg')
+    filter_index1 = findfirst(==(fnames[1]), String(i) for i in filternames(table1))
+    plot_data1 = [d[filter_index1] for d in data1]
+    # Repeat for second table
+    data2 = table2.(Teff, logg')
+    filter_index2 = findfirst(==(fnames[2]), String(i) for i in filternames(table2))
+    plot_data2 = [d[filter_index2] for d in data2]
+
+    f = Figure()
+    ax = Axis(f[1, 1], xlabel="log(Teff)", ylabel=L"$\text{log} \ g$")
+    ax.xreversed = true
+    p = heatmap!(ax, logTeff, logg, plot_data1 .- plot_data2; interpolate=false, colormap=:gist_rainbow) # :viridis) # , colormap=:cividis)
+    Colorbar(f[:, end+1], p)
+    return f, ax
+end
