669227e48cd62e31ee8dad06be1604eeb530586a
cgarling <chris.t.garling@gmail.com>
2025-07-24 19:04:35 -0400

    Fix Koester test

--- test/YBC/koester_tests.jl
+++ test/YBC/koester_tests.jl
@@ -1,5 +1,5 @@
 using Test: @test
-using BolometricCorrections.YBC.KoesterWD: KoesterWDYBCGrid, KoesterWDYBCTable, gridinfo, chemistry
+using BolometricCorrections.YBC.KoesterWD: KoesterWDYBCGrid, KoesterWDYBCTable, gridinfo, chemistry, dtype
 using StaticArrays: SVector
 
 @test ismissing(chemistry(KoesterWDYBCGrid))
@@ -9,5 +9,5 @@ grid = KoesterWDYBCGrid("acs_wfc")
 for Av in range(extrema(gridinfo.Av)...; step=0.1)
     table = grid(Av)
     @test table isa KoesterWDYBCTable
-    @test table(10_250.0, 7.33) isa SVector{12, BolometricCorrections.YBC.dtype}
+    @test table(10_250.0, 7.33) isa SVector{12, dtype}
 end
