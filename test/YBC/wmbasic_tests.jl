c408a9a721b3aeb4179cad4e6b4a4cd81774fd32
cgarling <chris.t.garling@gmail.com>
2025-07-24 18:53:37 -0400

    Test output types when interpolating
    Important these are uniform for `isochrone` methods in StellarTracks.jl

--- test/YBC/wmbasic_tests.jl
+++ test/YBC/wmbasic_tests.jl
@@ -1,6 +1,7 @@
 using Test: @test
 using BolometricCorrections
 using BolometricCorrections.YBC.WMbasic: WMbasicYBCGrid, WMbasicYBCTable, gridinfo
+using StaticArrays: SVector
 
 grid = WMbasicYBCGrid("acs_wfc")
 for mh in range(extrema(grid).MH[1], extrema(grid).MH[2]; length=10)
@@ -13,7 +14,7 @@ for mh in range(extrema(grid).MH[1], extrema(grid).MH[2]; length=10)
         @test Y(table) ≈ Y(chem, Z(table))
         @test X(table) ≈ X(chem, Z(table))
         for Mdot in range(extrema(table).Mdot...; length=10)
-            @test table(20_250.0, 3.51, Mdot) isa AbstractVector # technically SVector but ...
+            @test table(20_250.0, 3.51, Mdot) isa SVector{12, BolometricCorrections.YBC.dtype}
         end
     end
 end
